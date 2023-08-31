f556ddb7252019a6fb4949a23a981a75484df761
Nicolas Beaussart <nic.beaussart@gmail.com>
2023-08-31 13:45:08 +0200

    Remove eslint rule for explicit function return type
    The "@typescript-eslint/explicit-function-return-type" rule was removed from the eslint configuration. This decision was made to allow for inferred return types in TypeScript and complexity reduction.

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
   ],
   rules: {
-    "@typescript-eslint/explicit-function-return-type": "error",
     "@typescript-eslint/no-explicit-any": "error"
   }
 };
